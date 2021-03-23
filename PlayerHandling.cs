537a13fe2079201507034bcd74ca5145881f4509
Ritesh0610 <yadavitin197@gmail.com>
2021-03-23 11:17:55 +0530

    Submit

---
+++ PlayerHandling.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppTestingExercise
+{
+    [TestFixture]
+    class PlayerHandling
+    {
+        [Test]
+         public void TestPlayer()
+         {
+            foreach(var Pl in Program.playerList())
+            {
+                Assert.NotNull(Pl.PlayerId);
+                Assert.NotNull(Pl.PlayerName);
+                Assert.NotNull(Pl.PlayerTeam);
+            }
+         }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppTestingExercise
+{
+    class Program
+    {
+        public static List<Player> playerList()
+        {
+            List<Player> listPlayers = new List<Player>
+            {
+                new Player{PlayerId=1,PlayerName="Rohit Sharma",PlayerTeam="India"},
+                new Player{PlayerId=2,PlayerName="David Warner",PlayerTeam="Australia"},
+                new Player{PlayerId=3,PlayerName="Virat Kohli",PlayerTeam="India"},
+            };
+            return listPlayers;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
